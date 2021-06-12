e6ce29bdbf021c3e1340076b99a48a8a009c5e35
chrisdionisius <chrisdionisius@gmail.com>
2021-06-12 15:33:19 +0700

    add migrations

---
+++ database/migrations/2021_06_12_083052_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('jenis_transaksi');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('jumlah');
+            $table->integer('jumlah_awal');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
